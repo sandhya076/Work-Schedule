c727695cd52ab7a525a8b3ae5d7e1b81e3f7a3a6
Rohit11544 <brohit11544@gmail.com>
2022-01-25 18:13:29 +0530

    added url

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import bodyParser from "body-parser";
-import  Mongoose from "mongoose";
-import express  from "Express";
+import Mongoose from "mongoose";
+import express from "Express";
 import cors from "cors";
-import work from "./routes/work.js"
+import work from "./routes/work.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
-app.use(bodyParser.json({limit: "30mb",extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb" , extended : true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use('/work',work)
-const CONNECTION_URL = "mongodb+srv://sandhya:Sandhya@cluster0.trov7.mongodb.net/mySecondDatabase?retryWrites=true&w=majority";
+app.use("/work", work);
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 Mongoose.connect(CONNECTION_URL)
 
-
-.then(()=>app.listen(PORT,()=>console.log(`server running on :${PORT}`)))
-.catch((error)=>console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`server running on :${PORT}`)))
+  .catch((error) => console.log(error.message));
